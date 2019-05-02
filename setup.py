90de210baa3e87da81ceb90f50b526a4ebdde1ca
js-g <oblatespheroid@protonmail.com>
2019-05-01 22:04:56 -0400

    update with distutils

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
-import setuptools
+from distutils.core import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
+setup(
     name="python_world",
     version="0.0.1",
+    packages = ['python_world'],
     author="Cedd Burge",
     author_email="ceddlyburge@gmail.com",
     description="A function that returns 'world'",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/ceddlyburge/python_world",
-    packages=setuptools.find_packages(),
+    requires = ['numpy', 'pandas'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ]
 )
+
+# pip install git+https://github.com/OblateSpheroid/python_world#egg=python_world
+# python setup.py sdist --format gztar
